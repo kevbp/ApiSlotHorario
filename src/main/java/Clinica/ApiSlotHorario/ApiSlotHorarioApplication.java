45be39d1f4169ce7adfba3e17669a1b37caec2f4
Kevin Broncano <kevinbroncanop@gmail.com>
2025-11-20 17:28:12 -0500

    Nueva carga

---
+++ src/main/java/Clinica/ApiSlotHorario/ApiSlotHorarioApplication.java
@@ -0,0 +1,13 @@
+package Clinica.ApiSlotHorario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSlotHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSlotHorarioApplication.class, args);
+	}
+
+}
